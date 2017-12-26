848f7e6f901a9b9383bdb3fb2961797ac1f8d875
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-27 01:59:34 +0500

    adding of static lib. tpl

---
+++ ios/RNBSG/RNBSG.h
@@ -0,0 +1,12 @@
+//
+//  RNBSG.h
+//  RNBSG
+//
+//  Created by Apel Yl on 27/12/2017.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNBSG : NSObject
+
+@end
---
+++ ios/RNBSG/RNBSG.m
@@ -0,0 +1,12 @@
+//
+//  RNBSG.m
+//  RNBSG
+//
+//  Created by Apel Yl on 27/12/2017.
+//
+
+#import "RNBSG.h"
+
+@implementation RNBSG
+
+@end
