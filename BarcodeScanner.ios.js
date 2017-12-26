de6131da06435e31423a236ac07997ff6f6c6519
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-26 19:48:41 +0500

    ios js stuff with dummy view

---
+++ BarcodeScanner.ios.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class BarcodeScanner extends Component {
+  render() {
+    return (
+      <View style={{
+        marginTop: 20
+      }}>
+        <Text>
+          There will be ios impl.
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default BarcodeScanner;
