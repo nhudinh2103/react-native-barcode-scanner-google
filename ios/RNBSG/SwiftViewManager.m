93aef991ca441ae1c9c7c0fd57b26ad647128b4b
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-27 11:46:07 +0500

    adding of SwiftViewManager

---
+++ ios/RNBSG/SwiftViewManager.m
@@ -0,0 +1,22 @@
+//
+//  SwiftViewManager.m
+//  RNSwift
+//
+//  Created by Apel Yl on 21/12/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+#import <MapKit/MapKit.h>
+#import "SwiftViewManager.h"
+#import "RNBSG-Swift.h"
+
+@implementation SwiftViewManager
+
+RCT_EXPORT_MODULE()
+RCT_EXPORT_VIEW_PROPERTY(text, NSString)
+
+- (UIView *) view
+{
+  return [[SwiftView alloc] init];
+}
+
+@end
