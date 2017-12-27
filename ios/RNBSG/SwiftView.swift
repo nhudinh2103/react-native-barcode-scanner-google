f724a5b522dc4e6d6951c7b727dd01a4b1773463
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-27 11:42:21 +0500

    adding of SwiftView with label

--- ios/RNBSG/SwiftView.swift
+++ ios/RNBSG/SwiftView.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class SwiftView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 50))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        label.text = "This is initial text on the swift side"
+        addSubview(label)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init coder isn't supported")
+    }
+    
+    var text: String {
+        get {
+            return label.text ?? ""
+        }
+        
+        set(text) {
+            label.text = text
+        }
+    }
 }
