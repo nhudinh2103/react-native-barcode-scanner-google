ad57fd3a820829503909c78377962b927c71427e
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-27 11:33:08 +0500

    adding of swift file and bridging header 	new file:   ios/RNBSG/RNBSG-Bridging-Header.h 	new file:   ios/RNBSG/SwiftView.swift

---
+++ ios/RNBSG/SwiftView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftView.swift
+//  RNBSG
+//
+//  Created by Apel Yl on 27/12/2017.
+//
+
+import UIKit
+
+class SwiftView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
