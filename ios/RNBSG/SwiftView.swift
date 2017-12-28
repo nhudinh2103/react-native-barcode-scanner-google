04605b6858d03a38c2cd5b169e78fa6f3b20a724
Stanislav Bobykin <sbobykin@gmail.com>
2017-12-28 16:48:56 +0500

    code cleanup

--- ios/RNBSG/SwiftView.swift
+++ ios/RNBSG/SwiftView.swift
@@ -8,42 +8,19 @@ import UIKit
 class SwiftView: UIView {
     let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 50))
     let childVC = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! UITabBarController
-    //let childVC = CameraViewController()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        /*if let rootVC = UIApplication.shared.delegate?.window??.rootViewController {
-            rootVC.addChildViewController(childVC)
-            
-            addSubview(childVC.view)
-            
-            childVC.didMove(toParentViewController: rootVC)
-        }*/
-        
         if let rootVC = UIApplication.shared.delegate?.window??.rootViewController {
             rootVC.addChildViewController(childVC)
-            //childVC.swiftView = self
             addSubview(childVC.view)
             autoresizesSubviews = false
             childVC.didMove(toParentViewController: rootVC)
         }
-        
-        /*label.text = "This is initial text on the swift side"
-        addSubview(label)*/
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("Init coder isn't supported")
-    }
-    
-    var text: String {
-        get {
-            return label.text ?? ""
-        }
-        
-        set(text) {
-            label.text = text
-        }
     }
 }
