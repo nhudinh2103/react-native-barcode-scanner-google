42b8cbedeb3f366f5f2175b24676eeb02c65b396
Stanislav Bobykin <sbobykin@gmail.com>
2018-01-19 17:32:23 +0500

    Reexport

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import BarcodeScanner from './BarcodeScanner';
 export default BarcodeScanner;
 
-import {BarcodeType} from './BarcodeScanner';
-export {BarcodeType};
-
+export * from './BarcodeScanner';
