2d1bb87f9fd3b4717521c669ce54134b4a6d4d72
Stanislav Bobykin <sbobykin@gmail.com>
2018-04-29 18:43:09 +0500

    sed -> gsed

--- helpersios/replace.sh
+++ helpersios/replace.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 SYMBOLS="([^0-9a-zA-Z_])(RemoveLogSink|AddLogSink|FLAGS_[0-9a-zA-Z_]*)"
-SEDSYMS=$(echo $SYMBOLS | sed 's/|/\\\|/g' | sed 's/(/\\\(/g' | sed 's/)/\\\)/g')
+SEDSYMS=$(echo $SYMBOLS | gsed 's/|/\\\|/g' | gsed 's/(/\\\(/g' | gsed 's/)/\\\)/g')
 
 find node_modules/react-native/third-party \
       -iname "*.c" -o -iname "*.cc" \
       -o -iname "*.in" -o -iname "*.h" \
       | xargs egrep -l "$SYMBOLS" \
-      | xargs sed -i "s/$SEDSYMS/\1REACT\2/g"
+      | xargs gsed -i "s/$SEDSYMS/\1REACT\2/g"
